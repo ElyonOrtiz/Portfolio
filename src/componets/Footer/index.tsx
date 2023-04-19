6446b33035376f7e1b7c159e24ee50d1630efa71
Elyon <elyon.ortiz08@gmail.com>
2023-04-19 16:55:11 -0400

    page index completed successfully

---
+++ src/componets/Footer/index.tsx
@@ -0,0 +1,33 @@
+import { Container } from './styles';
+import {
+  AiOutlineGithub,
+  AiOutlineLinkedin,
+} from 'react-icons/ai';
+
+function handleScrollTop {
+  wi
+}
+
+
+export function Footer() {
+  function handleRedirect(url: string) {
+    window.open(url);
+  }
+  return (
+    <Container>
+      <div className='container'>
+        <button>
+          Voltar ao topo
+        </button>
+        <section>
+          <AiOutlineGithub
+            onClick={ () => handleRedirect('https://github.com/ElyonOrtiz') }
+          />
+          <AiOutlineLinkedin
+            onClick={ () => handleRedirect('https://www.linkedin.com/in/elyon-ortiz-888845243/') }
+          />
+        </section>
+      </div>
+    </Container>
+  );
+}
